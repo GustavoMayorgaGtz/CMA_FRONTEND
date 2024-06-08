eeffaad5b5b10fdefbe9adaa457666cdbd0174ef
Gustavo Mayorga <redvioletoficial@gmail.com>
2024-06-07 22:55:53 -0600

    responsive in dashboard menu

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 //--Produccion
-export const server = "https://gustvomayorga.com.mx/cma_server/";
+// export const server = "https://gustvomayorga.com.mx/cma_server/";
 //--Development
-// export const server = "http://localhost:5000/CMA_SERVER/";
+export const server = "http://localhost:5000/CMA_SERVER/";
