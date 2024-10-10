5dcefe4e4f00c4b6e5fa8c903a064e9aa395b42b
GustavoMayorgaGtz <redvioletoficial@gmail.com>
2024-10-10 13:29:02 -0600

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 //--Produccion
-export const server = "https://gustvomayorga.com.mx/cma_server_development/";
-export const ws_server = "ws://gustvomayorga.com.mx/cma_server_development/"
+// export const server = "https://gustvomayorga.com.mx/cma_server_development/";
+// export const ws_server = "ws://gustvomayorga.com.mx/cma_server_development/"
 // export const server = "http://localhost:5000/cma_server/";
+export const ws_server = "ws://cmaserver-f81f9d905f26.herokuapp.com/"
+export const server = "https://cmaserver-f81f9d905f26.herokuapp.com/cma_server_development/";
 
