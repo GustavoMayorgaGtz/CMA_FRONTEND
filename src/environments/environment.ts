f319bf80d76a8e6ef2ac6cdd4c196cedda57c19c
GustavoMayorgaGtz <redvioletoficial@gmail.com>
2025-03-05 08:38:17 -0600

    save

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,11 @@
 // export const server = "https://gustvomayorga.com.mx/cma_server/";
 
 // //--Development
-export const ws_server = "ws://localhost:5000/"
-export const server = "http://localhost:5000/cma_server_development/";
+// export const ws_server = "ws://localhost:5000/"
+// export const server = "http://localhost:5000/cma_server_development/";
 
-// export const ws_server = "wss://www.cmasystems.com.mx/"
-// export const server = "https://www.cmasystems.com.mx/cma_server_development/";   
+export const ws_server = "wss://www.cmasystems.com.mx/"
+export const server = "https://www.cmasystems.com.mx/cma_server_development/";   
 
 // export const ws_server = "ws://192.168.100.163:5000/"
 // export const server = "http://192.168.100.163:5000/cma_server_development/";
